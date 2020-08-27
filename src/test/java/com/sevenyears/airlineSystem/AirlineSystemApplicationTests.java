b993e937177322ab149f6753a7dbd91f15f19dbd
Marco7years <marcosettanni08@gmail.com>
2020-08-27 17:52:06 +0200

    Spring boot initializr

---
+++ src/test/java/com/sevenyears/airlineSystem/AirlineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sevenyears.airlineSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
